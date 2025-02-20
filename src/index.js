a437a1e0a4b22dc5000986b8e3ff1bd441c818be
techuva2024 <techuva2024@gmail.com>
2025-02-20 10:44:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Server is running!");
