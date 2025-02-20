06e3cd6453606eab7c4d36869d0ceb2108aa1700
techuva2024 <techuva2024@gmail.com>
2025-02-20 12:02:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-app.use(cors());
 
 
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(cors({
