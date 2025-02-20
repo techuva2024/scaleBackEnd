ded50a197876bb635154e1ad2557d32a67cea725
techuva2024 <techuva2024@gmail.com>
2025-02-20 12:01:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 
 app.get("/", (req, res) => {
   res.send("Server is running!");
